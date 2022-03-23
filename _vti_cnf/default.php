4a865d64b24a7b200a0c53ea131cef4794f06027
Sam Grossick <35524098+samgrossick@users.noreply.github.com>
2022-03-23 16:00:06 +0000

    Initial Commit
    From D Gibson

---
+++ _vti_cnf/default.php
@@ -0,0 +1,20 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|15 Jan 2018 20:54:44 -0000
+vti_extenderversion:SR|6.0.2.5516
+vti_author:SR|DHGLAPTOP\\David
+vti_modifiedby:SR|Olivia-PC\\Olivia
+vti_timecreated:TR|29 Apr 2016 16:03:34 -0000
+vti_title:SR|Welcome to <? print $host; ?>
+vti_backlinkinfo:VX|
+vti_cacheddtm:TX|15 Jan 2018 20:54:44 -0000
+vti_filesize:IR|8062
+vti_cachedtitle:SR|Welcome to <? print $host; ?>
+vti_cachedbodystyle:SR|<body>
+vti_cachedlinkinfo:VX|Q|http://www.000webhost.com/images/index/styles.css H|http://members.000webhost.com/ H|http://www.000webhost.com/forum/ H|http://www.000webhost.com/faq.php H|http://www.000webhost.com/includes/tos.php H|http://www.000webhost.com/includes/tos.php H|< S|http://www.000webhost.com/images/index/dirup.png H|< S|http://www.000webhost.com/images/index/folder.png H|< S|http://www.000webhost.com/images/index/< H|http://www.hosting24.com/ H|http://www.000webhost.com/
+vti_cachedsvcrellinks:VX|NQHS|http://www.000webhost.com/images/index/styles.css NHHS|http://members.000webhost.com/ NHHS|http://www.000webhost.com/forum/ NHHS|http://www.000webhost.com/faq.php NHHS|http://www.000webhost.com/includes/tos.php NHHS|http://www.000webhost.com/includes/tos.php DHUS|< NSHS|http://www.000webhost.com/images/index/dirup.png DHUS|< NSHS|http://www.000webhost.com/images/index/folder.png DHUS|< NSHS|http://www.000webhost.com/images/index/< NHHS|http://www.hosting24.com/ NHHS|http://www.000webhost.com/
+vti_cachedneedsrewrite:BR|false
+vti_cachedhasbots:BR|false
+vti_cachedhastheme:BR|false
+vti_cachedhasborder:BR|false
+vti_metatags:VR|HTTP-EQUIV=Content-Type text/html;\\ charset=iso-8859-1
+vti_charset:SR|iso-8859-1
